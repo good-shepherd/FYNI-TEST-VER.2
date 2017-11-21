a011a75c95692e234eb75850ab95d793abc09ef5
frannacdh <frannacdh@gmail.com>
2017-11-21 17:28:32 +0900

    ver 1.16

--- src/main/java/com/fyni/interceptor/LoginInterceptor.java
+++ src/main/java/com/fyni/interceptor/LoginInterceptor.java
@@ -1,7 +1,20 @@
 package com.fyni.interceptor;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter{
-
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView mv) throws Exception{
+		
+	}
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+		return true;
+	}
 }
