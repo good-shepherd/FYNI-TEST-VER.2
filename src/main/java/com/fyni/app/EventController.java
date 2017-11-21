1689cb7440320c735beda8ce42ed584cc83d5f8b
August <dazedbj@gmail.com>
2017-11-21 11:03:30 +0900

    ver 1.3
    some changes (controller)

---
+++ src/main/java/com/fyni/app/EventController.java
@@ -0,0 +1,31 @@
+package com.fyni.app;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fyni.domain.EventDTO;
+import com.fyni.service.EventServiceImpl;
+
+@RestController
+public class EventController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private EventServiceImpl service;
+
+	@RequestMapping(value = "/listAll.do", method = RequestMethod.GET)
+	public ModelAndView searchAll(ModelAndView mav) throws Exception {
+		List<EventDTO> list = service.eventReadAll();
+		mav.addObject(list);
+		mav.setViewName("list");
+		return mav;
+	}
+}
