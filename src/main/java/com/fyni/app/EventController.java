df8f68dc735fadf15528c36d9aeca481ab9ef2f6
August <dazedbj@gmail.com>
2017-11-29 16:55:32 +0900

    ver 11-29 4:55

--- src/main/java/com/fyni/app/EventController.java
+++ src/main/java/com/fyni/app/EventController.java
@@ -2,6 +2,8 @@ package com.fyni.app;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.fyni.domain.EventDTO;
 import com.fyni.service.EventServiceImpl;
 
@@ -30,14 +31,15 @@ public class EventController {
 
 	@RequestMapping(value = "eventCreate.do", method = RequestMethod.POST)
 	public String eventCreate(String event_Title, String event_Content, String event_WhenBegins, String event_WhenEnds,
-			MultipartFile event_Picture) throws Exception {
+			MultipartFile event_Picture, HttpSession session) throws Exception {
 		System.out.println(event_Title);
 		System.out.println(event_Content);
 		System.out.println(event_WhenBegins);
 		System.out.println(event_WhenEnds);
+		System.out.println(session.getAttribute("user_ID"));
 		// System.out.println(event_Picture);
 		EventDTO dto = new EventDTO();
-		
+
 		// 2017-01-01T01:00 -> YYYY-MM-DD HH:MM:SS
 		StringBuffer sb = new StringBuffer();
 		sb.append(event_WhenBegins.substring(0, 10)).append(" ").append(event_WhenBegins.substring(11)).append(":00");
@@ -45,7 +47,7 @@ public class EventController {
 		sb = new StringBuffer();
 		sb.append(event_WhenEnds.substring(0, 10)).append(" ").append(event_WhenEnds.substring(11)).append(":00");
 		String ends = sb.toString();
-		
+
 		dto.setEvent_Title(event_Title);
 		dto.setEvent_Content(event_Content);
 		dto.setEvent_WhenBegins(begins);
