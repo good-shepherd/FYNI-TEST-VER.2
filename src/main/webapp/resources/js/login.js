6d5c4c035e4e9271b317825b35662378b92f0fd4
frannacdh <frannacdh@gmail.com>
2017-11-29 14:11:50 +0900

    ver 14:11 - 11/29 CDH

--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -41,74 +41,79 @@ var loginform =
 '</div>'
 
 var signupform = 
-	'<div class="col-md-4 col-md-offset-4"> '+
-	'<div class="row">' +
-	'<div class="main">' +
-	'  <h3>Sign Up for FYNI</h3>' +
-	'  <div class="row"> ' + 
-	'    <div class="col-xs-6 col-sm-6 col-md-6">'+
-	'<a href="#" class="btn btn-lg btn-primary btn-block">Facebook</a>' +
-	'</div>'+
-	'<div class="col-xs-6 col-sm-6 col-md-6">' + 
-	'<a href="#" class="btn btn-lg btn-info btn-block">Google</a>' +
-	'</div>'+
-	'</div>'+
-	'<div class="login-or">'+
-	'<hr class="hr-or">'+
-	'<span class="span-or">or</span>'+
-	'</div>'+
-	'<form role="form">'+
-	'<div class="form-group">'+
-	'<label for="signup-user-id">Email</label>'+
-	'<input type="text" class="form-control" id="signup-user-id"'+
-	'name="user_ID">'+
-	'</div>'+
-	'<div class="form-group">'+
-	'<label for="signup-user-pwd">Password</label>'+
-	'<input type="password" class="form-control" id="signup-user-pwd"'+
-	'name="user_PWD">'+
-	'</div>'+
-	'<div class="form-group">'+
-	'<label for="signup-user-nickname">Nickname</label>'+
-	' <input type="text" class="form-control" id="signup-user-nickname"'+
-	'name=""user_Nickname"">'+
-	'</div>'+
-	'<div class="form-group">'+
-	' <label for="inputPassword">Default Location</label><small> optional</small>'+
-	'<input type="text" style="border-radius:3px" id="signup-user-address"'+
-	'name="user_Address" disabled>'+
-	'<button class="btn-primary pull-right" style="border-radius:3px" id="findaddressbtn">Find address</button>'+
-	'</div>'+
-	'<div class="checkbox pull-right">'+
-	'<label><input type ="checkbox">프로필 공개</label>' + 
-	'</div>'+ 
-	'<button type="button" class="btn btn-primary" id="signupbtn">'+
-	'Sign Up ' + 
-	'  </button>' + 
-	'</form> '+
-	'</div>' +
-	'</div>' + 
-	'</div>';
+'	 <div class= " col-md-4 col-md-offset-4">' +
+'<div class="row" >' +
+'  <div class="main"> '+
+'    <h3>Sign Up for FYNI</h3> '+
+'    <div class="row">'+
+'      <div class="col-xs-6 col-sm-6 col-md-6">'+
+'        <a href="#" class="btn btn-lg btn-primary btn-block">Facebook</a>'+
+'      </div>'+
+'      <div class="col-xs-6 col-sm-6 col-md-6">'+
+'        <a href="#" class="btn btn-lg btn-info btn-block">Google</a>'+
+'      </div>'+
+'    </div>'+
+'    <div class="login-or">'+
+'      <hr class="hr-or">'+
+'      <span class="span-or">or</span>'+
+'    </div>'+
+'    <form role="form" id = "f" name="f">'+
+'      <div class="form-group">'+
+'        <label for="signup-user-id">Email</label>'+
+'        <input type="text" class="form-control" id="signup-user-id"'+
+'				 name="user_ID">'+
+'      </div>'+
+'      <div class="form-group">'+
+'        <label for="signup-user-pwd">Password</label>'+
+'        <input type="password" class="form-control" id="signup-user-pwd"'+
+'				 name="user_PWD">'+
+'      </div>'+
+'		<div class="form-group">'+
+'        <label for="signup-user-nickname">Nickname</label>'+
+'        <input type="text" class="form-control" id="signup-user-nickname"'+
+'				 name="user_Nickname">'+
+'      </div>'+
+'		<div class="form-group"> '+
+'        <label for="inputPassword">Default Location</label><small> optional</small>'+
+'        <input type="text" style="border-radius:3px" id="signup-user-address"'+
+'		  name="user_Address" disabled>'+
+'		  <input type="button" class="btn-primary pull-right" style="border-radius:3px" id="findaddressbtn"'+ 
+'		  data-toggle="modal" data-target="#mapModal" value="Find address">'+
+'      </div>'+
+'		<div class="checkbox pull-right">'+
+'        <label><input type ="checkbox">프로필 공개</label>'+
+'      </div>'+
+'      <input type="button" class="btn btn-primary" id="signupbtn" value="Sign Up">'+
+'    </form>'+
+'  </div>'+
+'</div>'+
+'</div>'+
+'	<div class="modal fade" id="mapModal">'+
+'	  <div class="modal-dialog modal-lg">'+
+'	    <div class="modal-content">'+
+'	      <!-- Modal Header -->'+
+'	      <div class="modal-header">'+
+'	        <h4 class="modal-title">Select your Default Address</h4>'+
+'	      </div>'+
+'	      <div class="modal-body col-md-6">'+
+'	      </div>'+
+'	      <div class="modal-footer">'+
+'	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'+
+'	      </div>'+
+'	    </div>'+
+'	  </div>'+
+'	</div>';
 
-function cngsignup(){
-	$.ajax({
-		context : $('#here-to-change').html(signupform)
-	});
-}
-
-function cnglogin(){
-	$.ajax({
-		context : $('#here-to-change').html(loginform)
-	});
-}
 
 $(document).ready(function(){
 	$('#loginbtn').on('click',function(){
+		$('#f').action = "login.do";
+		$('#f').method = "post";
 		$('#f').submit();
 	});
-	
 	$('#signupbtn').on('click',function(){
-		$('#f').serialize();
+		$('#f').action = "signup.do";
+		$('#f').method = "post";
 		$('#f').submit();
 	});
 });
